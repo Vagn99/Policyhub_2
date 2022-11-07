655940334dda5f4a5472a38d687b7b5c84575fc1
Vagn99 <christoffer.lorenzen@outlook.com>
2022-11-07 09:48:15 +0100

    Initial commit

---
+++ src/main/java/Group6/Policyhub/PolicyhubApplication.java
@@ -0,0 +1,13 @@
+package Group6.Policyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Group6/Policyhub/PolicyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package Group6.Policyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
